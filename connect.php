fdb5edf689f89517aa3e7d4119cb53d1e771a280
snowmintowo <starlitsky0612@gmail.com>
2018-06-25 16:52:12 +0800

    omit comment

--- connect.php
+++ connect.php
@@ -1,9 +1,5 @@
 <?php session_start(); ?>
-<!--上方語法為啟用session，此語法要放在網頁最前方-->
-
 <?php
-//連接資料庫
-//只要此頁面上有用到連接MySQL就要include它
 include("head_line.inc.php");
 include("mysql_connect.inc.php");
 mysqli_query($conn, "SET NAMES utf8");
@@ -16,10 +12,9 @@ $row = mysqli_fetch_row($result);
 
 $ch = '$2y$10$GnTzWKFrQoWVrbdsKzCuteUMoPWE.urPrDfZluN0mbSmbi2xrWIym';
 if($_POST['button_admin'] != null && password_verify($_POST['pw_admin'], $ch)) {
-    //$2y$10$GnTzWKFrQoWVrbdsKzCuteUMoPWE.urPrDfZluN0mbSmbi2xrWIym
     $_SESSION['ID'] = $id;
     $_SESSION['PS'] = $row[4];
-    if($id != null && $pw != null && $row[1] == $id && password_verify($pw, $row[4])) { //$row[4] == $hash
+    if($id != null && $pw != null && $row[1] == $id && password_verify($pw, $row[4])) {
         $_SESSION['admin'] = $_POST['pw_admin'];
         echo "<img src = './pic/lloginsuccess.png' width='90%' style='display:block; margin:auto;'>";
         echo '<meta http-equiv=REFRESH CONTENT=1;url=delete.php>';
@@ -34,17 +29,11 @@ else if($_POST['pw_admin'] != null && !password_verify($_POST['pw_admin'], $ch))
     echo '<meta http-equiv=REFRESH CONTENT=1;url=index.php>';
 }
 else if($_POST['button'] != null){
-    if($id != null && $pw != null && $row[1] == $id && password_verify($pw, $row[4])) { //$row[4] == $hash
-            //將帳號寫入session，方便驗證使用者身份
-        
-        //echo "$row[3] $row[2] <br>";
-        //echo '登入成功!';
-        //echo "<script>alert('$id $row[1] $row[4]');</script>";
+    if($id != null && $pw != null && $row[1] == $id && password_verify($pw, $row[4])) {
         echo "<img src = './pic/lloginsuccess.png' width='90%' style='display:block; margin:auto;'>";
         echo '<meta http-equiv=REFRESH CONTENT=1;url=Top.php>';
     }
     else {
-            //echo "<script>alert('$id $row[1] $row[4]');</script>";
             echo "<img src = './pic/lloginFaild.png' width='90%' style='display:block; margin:auto;'>";
             echo '<meta http-equiv=REFRESH CONTENT=1;url=index.php>';
     }
